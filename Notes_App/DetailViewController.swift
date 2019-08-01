10d7e4d647dbcb18529f918f4a7e08d5cd43dd26
Yuvaraj <yuvaraja.krishnamourthy@freshworks.com>
2019-08-01 15:32:18 +0530

    Fixed few review comments

--- Notes_App/DetailViewController.swift
+++ Notes_App/DetailViewController.swift
@@ -8,33 +8,42 @@
 
 import UIKit
 
+@available(iOS 11.0, *)
 class DetailViewController: UIViewController {
 
+   
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
     var text: String = ""
+    var titleText: String = ""
     var masterView:ViewController!
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.text = text
+        textField.text = titleText
         self.navigationItem.largeTitleDisplayMode = .never
+    
     }
     
-    func setText(t:String)
+    func setText(t:String,t1:String)
     {
         text = t
+        titleText = t1
         if isViewLoaded{
             textView.text = text
+            textField.text = titleText
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        textView.becomeFirstResponder()
+        textField.becomeFirstResponder()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        masterView.newRowText = textView.text
+        masterView.newRowText = textView.text!
+        masterView.newRowText1 = textField.text!
         textView.resignFirstResponder()
     }
 
--- Notes_App/ViewController.swift
+++ Notes_App/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
-
+import CoreData
+@available(iOS 11.0, *)
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var table: UITableView!
-    var data:[String] = []
+    var data2 = [NSManagedObject]()
     var selectedRow: Int = -1
     var newRowText:String = ""
-    var fileURL:URL!
+    var newRowText1:String = ""
+    var notes  = [Notes]()
+    var ip: IndexPath = IndexPath(row: 0, section: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +26,10 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         table.delegate = self
         self.title = "NOTES"
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationItem.largeTitleDisplayMode = .always
+        //self.navigationItem.largeTitleDisplayMode = .always
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
         self.navigationItem.rightBarButtonItem = addButton
         self.navigationItem.leftBarButtonItem = editButtonItem
-        let baseURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        fileURL = baseURL.appendingPathComponent("notes.txt")
         load()
     }
     
@@ -37,12 +38,18 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         if selectedRow == -1 {
             return
         }
-        data[selectedRow] = newRowText
-        if newRowText == "" {
-            data.remove(at: selectedRow)
-        }
-        table.reloadData()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        print(selectedRow)
+        data2[selectedRow].setValue(newRowText, forKey: "newRowText2")
+        data2[selectedRow].setValue(newRowText1, forKey: "newRowText3")
+        if newRowText == ""{
+            managedContext.delete(data2[selectedRow])
+            data2.remove(at: selectedRow)
+       }
         save()
+        load()
+        table.reloadData()
     }
     
     @objc func addNote()
@@ -51,34 +58,49 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         {
             return
         }
-        let name:String = ""
-        data.insert(name, at:0)
-        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        let name: String = ""
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Notes", in: managedContext)
+        let newRowText2 = NSManagedObject(entity: entity!, insertInto: managedContext)
+        newRowText2.setValue(name, forKey: "newRowText2")
+        newRowText2.setValue(name, forKey: "newRowText3")
+        data2.append(newRowText2)
+        save()
+        let indexPath:IndexPath = IndexPath(row: data2.count-1, section: 0)
         table.insertRows(at: [indexPath], with: .automatic)
         table.selectRow(at: indexPath, animated: true, scrollPosition: .none)
         self.performSegue(withIdentifier: "detail", sender: nil)
-        save()
+        return
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return data2.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
+        if data2[indexPath.row].value(forKey: "newRowText3") as! String == ""
+        {
+            cell.textLabel?.text = data2[indexPath.row].value(forKey: "newRowText2") as? String
+            return cell
+        }
+        else
+        {
+            cell.textLabel?.text = data2[indexPath.row].value(forKey: "newRowText3") as? String
+           return cell
+        }
     }
     
     override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated:animated)
+        super.setEditing(editing, animated: animated)
         table.setEditing(editing, animated: animated)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        data.remove(at: indexPath.row)
-        table.deleteRows(at: [indexPath], with: .fade)
-        save()
+        setIndexPath(i: indexPath)
+        load()
+        dialogOKCancel(i1:indexPath)
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -89,15 +111,16 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         let detailView:DetailViewController = segue.destination as! DetailViewController
         selectedRow = table.indexPathForSelectedRow!.row
         detailView.masterView = self
-        detailView.setText(t: data[selectedRow])
+        load()
+        detailView.setText(t:(data2[selectedRow].value(forKey: "newRowText2") as! String?)!,t1:(data2[selectedRow].value(forKey: "newRowText3") as! String?)!)
     }
     
     func save()
     {
-        //UserDefaults.standard.set(data, forKey: "notes")
-        let a = NSArray(array: data)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
         do {
-            try a.write(to: fileURL)
+           try managedContext.save()
         } catch  {
             print("Error failing to write the file")
         }
@@ -105,12 +128,47 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     func load()
     {
-        //if let loadedData:[String] = UserDefaults.standard.value(forKey: "notes") as? [String]
-        if let loadedData:[String] = NSArray(contentsOf: fileURL) as? [String]
-        {
-            data = loadedData
-            table.reloadData()
-        }
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let managedContext = appDelegate.persistentContainer.viewContext
+    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Notes")
+    do {
+    try notes = managedContext.fetch(fetchRequest) as! [Notes]
+        data2 = notes as [NSManagedObject]
+    }
+    catch {
+    print("error")
+   }
+    table.reloadData()
+        
+    }
+    
+    func dialogOKCancel(i1:IndexPath)    {
+        
+        let alert = UIAlertController(title: "Do you want to delete the note?", message: "Note will be deleted", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Delete", style: .default, handler: { action in self.delete()})
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func delete() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let j = getIndexPath()
+        managedContext.delete(data2[j.row])
+        data2.remove(at: j.row)
+        table.deleteRows(at: [j], with: .fade)
+        save()
+    }
+    
+    func setIndexPath(i:IndexPath )
+    {
+     ip = i
+    }
+    func getIndexPath() -> IndexPath
+    {
+        return ip
     }
     
     override func didReceiveMemoryWarning() {
